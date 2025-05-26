1d5163c722aa2e5bbb1ef9d5c26f10fa5f9adea0
leomelegari <leo_melegari@outlook.com>
2025-05-26 17:57:09 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div className="flex items-center justify-baseline bg-black text-2xl">
-      Home
-    </div>
-  );
+  return <Button>BUTÃO</Button>;
 }
