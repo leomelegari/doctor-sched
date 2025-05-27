dc2a39dfc86df2a2ba67b711f2745baf6937e3ee
leomelegari <leo_melegari@outlook.com>
2025-05-27 15:39:41 -0300

    feat: implement authentication and sign-out functionality

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+import SignOutButton from "./components/sign-out-button";
+import { redirect } from "next/navigation";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  return (
+    <div>
+      <h1>{session?.user.name}</h1>
+      <SignOutButton />
+    </div>
+  );
 };
 
 export default DashboardPage;
