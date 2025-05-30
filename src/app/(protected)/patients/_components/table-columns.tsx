1f2f6f33d8b77111841d4a4b3645b5f10fff32ac
leomelegari <leo_melegari@outlook.com>
2025-05-30 15:24:17 -0300

    feat: implement patient management features with upsert functionality, including patient form, table, and actions

---
+++ src/app/(protected)/patients/_components/table-columns.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { patientsTable } from "@/db/schema";
+import PatientsTableActions from "./table-actions";
+
+type Patient = typeof patientsTable.$inferSelect;
+
+export const patientsTableColumns: ColumnDef<Patient>[] = [
+  {
+    id: "name",
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    id: "email",
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    id: "phoneNumber",
+    accessorKey: "phoneNumber",
+    header: "Telefone",
+    cell: (params) => {
+      const patient = params.row.original;
+      const phoneNumber = patient.phoneNumber;
+      if (!phoneNumber) return "";
+      const formatted = phoneNumber.replace(
+        /(\d{2})(\d{5})(\d{4})/,
+        "($1) $2-$3",
+      );
+      return formatted;
+    },
+  },
+  {
+    id: "gender",
+    accessorKey: "gender",
+    header: "Sexo",
+    cell: (params) => {
+      const patient = params.row.original;
+      return patient.gender === "male" ? "Masculino" : "Feminino";
+    },
+  },
+  {
+    id: "actions",
+    cell: (params) => {
+      const patient = params.row.original;
+      return <PatientsTableActions patient={patient} />;
+    },
+  },
+];
